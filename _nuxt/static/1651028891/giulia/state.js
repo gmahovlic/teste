0495d0f545606c56f00cd5551241c0feaf91bd89
gmahovlic <gmahovlic@users.noreply.github.com>
2022-04-27 03:08:12 +0000

    deploy: 3d4fca19bf7b9d05694d02b9d0c7029d6de3932a

---
+++ _nuxt/static/1651028891/giulia/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fteste\u002F_nuxt\u002Fstatic\u002F1651028891",layout:"default",error:a,serverRendered:true,routePath:"\u002Fgiulia",config:{_app:{basePath:"\u002Fteste\u002F",assetsPath:"\u002Fteste\u002F_nuxt\u002F",cdnURL:a}}}}(null));
